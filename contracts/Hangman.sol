ad93a2f9a865726d441f692895740fc64225c407
brian <bchou515@gmail.com>
2019-05-26 11:43:11 -0400

    trying assembly

---
+++ contracts/Hangman.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.5.0;
+
+contract Hangman {
+    address public playerAddress; // public gets an automatic getter
+    bytes private solution;
+    uint public maxGuesses;
+    uint public currentGuesses;
+    uint public playerInput; // 256 bits
+
+    constructor(bytes memory _solution, uint _maxGuesses) public {
+        playerAddress = msg.sender;
+        solution = _solution;
+        maxGuesses = _maxGuesses;
+        currentGuesses = 0;
+    }
+
+    function makeCharGuess(byte _character) external {
+        require(currentGuesses < maxGuesses, "no more guesses available");
+        require(playerInput < 2**(solution.length), "solution is found"); // becareful of overflow
+
+        currentGuesses += 1; // increment currentguesses by 1
+
+        // uint check = 0; // check is the reverse representation of string inputs
+        for (uint i = 0; i < solution.length; i++) {
+            if (solution[i] == _character) {
+                playerInput = computeGuess(i);
+            }
+        }
+    }
+
+    function computeGuess(uint i) private view returns (uint output) {
+        assembly {
+            //reference global
+            output := or(sload(playerInput_slot), exp(2, i))
+        }
+    }
+
+    function makeWordGuess() external {
+
+        // increment currentgueses by 1
+    }
+
+    function getNumberOfCharacters() public {
+
+    }
+}

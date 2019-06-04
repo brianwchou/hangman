1aedf9dcbe4906fff13e4eecd2fe765ebd43702c
Ethan Wessel <ethan.wessel@fluidity.io>
2019-06-04 15:50:32 -0400

    pull data from input fields and send to contract

--- web/src/Game.js
+++ web/src/Game.js
@@ -5,32 +5,55 @@ function Game() {
 
   const [ethersContext, setEthersContext] = useContext(EthersContext);
   const [hangmanString, setHangmanString] = useState("");
+  const [charGuess, setCharGuess] = useState("");
+  const [wordGuess, setWordGuess] = useState("");
+  const [currentGuesses, setCurrentGuesses] = useState(0);
+  const [maxGuesses, setMaxGuesses] = useState(0);
 
   async function guessChar() {
     if(ethersContext.contract === undefined) {
       console.log("no contract deployed");
       return;
+    } else if (charGuess === "") {
+      console.log("no character in box");
+      return;
+    } else if (charGuess.length > 1) {
+      console.log("1 character only");
+      return;
     }
+
     console.log(ethersContext.contract);
-    let guess = window.web3.fromAscii("h");
+    let guess = window.web3.fromAscii(charGuess);
     console.log(guess);
     let tx = await ethersContext.contract.makeCharGuess(guess);
     console.log(tx);
     await tx.wait();
 
+    let numerator = await ethersContext.contract.currentGuesses();
+    setCurrentGuesses(numerator.toNumber());
+    let denominator = await ethersContext.contract.maxGuesses();
+    setMaxGuesses(denominator.toNumber());
   }
 
   async function guessWord() {
     if(ethersContext.contract === undefined) {
       console.log("no contract deployed");
       return;
+    }  else if (wordGuess === "") {
+      console.log("no word in box");
+      return;
     }
     console.log(ethersContext.contract);
-    let guess = window.web3.fromAscii("hel");
+    let guess = window.web3.fromAscii(wordGuess);
     console.log(guess);
     let tx = await ethersContext.contract.makeWordGuess(guess);
     console.log(tx);
     await tx.wait();
+
+    let numerator = await ethersContext.contract.currentGuesses();
+    setCurrentGuesses(numerator.toNumber());
+    let denominator = await ethersContext.contract.maxGuesses();
+    setMaxGuesses(denominator.toNumber());
   }
 
   return (
@@ -41,6 +64,8 @@ function Game() {
           type = "text"
           name = "character"
           placeholder = "character"
+          maxLength="1"
+          onChange={ e => setCharGuess(e.target.value) }
       />&nbsp;
       <button type="button" onClick={ guessChar }>Guess Character</button>
       <br />
@@ -49,9 +74,11 @@ function Game() {
           type = "text"
           name = "character"
           placeholder = "word"
+          onChange={ e => setWordGuess(e.target.value) }
       />&nbsp;
       <button type="button" onClick={ guessWord }>Guess Word</button>
       <br />
+      { currentGuesses } / { maxGuesses } guesses left
     </div>
   );
 }

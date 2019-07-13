cb25aafa24bda8bb706f22418738a4815a866b39
Ethan Wessel <ejwessel@gmail.com>
2019-07-13 18:40:56 -0400

    prepare testing

--- test/StartGame.test.js
+++ test/StartGame.test.js
@@ -1,5 +1,8 @@
 const StartGame = artifacts.require("StartGame");
 const Hangman = artifacts.require("Hangman");
+const Oracle = artifacts.require("Oracle");
+const LinkToken = artifacts.require("LinkToken");
+const MockContract = artifacts.require("MockContract");
 const helper = require('ganache-time-traveler');
 const truffleAssert = require('truffle-assertions');
 const utils = require('./utils.js');
@@ -10,13 +13,26 @@ const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
 contract('StartGame', async (accounts) => {
 
   let startGame;
+  let mockLinkToken;
+  let mockOracle;
   let linkTokenAddress = accounts[2];
   let oracleAddress = accounts[3];
   let url = "https://en.wikipedia.org/api/rest_v1/page/random/title";
   let path = "items[0].title";
 
   before('deploy StartGame', async() => {
-      startGame = await StartGame.new(linkTokenAddress, oracleAddress, url, path);
+      let linkTokenTemplate = await LinkToken.new();
+      let mockLinkToken = await MockContract.new();
+
+      let oracleTemplate = await Oracle.new(0);
+      let mockOracle = await MockContract.new();
+
+      //mock LinkToken.sendChainlinkRequest()
+      let mockLink_sendChainlinkRequest = 
+        linkTokenTemplate.contract.methods.sendChainlinkRequest(0, 0).encodeABI();
+      await mockLinkToken.givenCalldataReturn(mockLink_sendChainlinkRequest, "testing");
+
+      startGame = await StartGame.new(mockLinkToken.address, mockOracle.address, url, path);
   });
 
   describe("Test initial values", async () => {

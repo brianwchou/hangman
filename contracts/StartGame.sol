ad0b465abcfe2ba0f132c9d025ae1889af7c92c7
Ethan Wessel <ejwessel@gmail.com>
2019-07-13 19:41:11 -0400

    use lower version mock and mock the link token

--- contracts/StartGame.sol
+++ contracts/StartGame.sol
@@ -5,10 +5,19 @@ import "chainlink/contracts/ChainlinkClient.sol";
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract StartGame is ChainlinkClient, Ownable {
+    struct Game {
+        address player;
+        address game;
+    }
+
     string public url;
     string public path;
     address public oracleAddr;
     bytes32 public constant CHAINLINK_JOB_ID = "013f25963613411badc2ece3a92d0800";
+    mapping(bytes32 => GameRequest) public requestIdToGame;
+
+    //map job id to game struct
+    //game struct will have address of the owner of that game and the hangman address
 
     constructor(address _link, address _oracle, string _url, string _path) public {
         setChainlinkToken(_link);
@@ -17,20 +26,32 @@ contract StartGame is ChainlinkClient, Ownable {
         path = _path;
     }
 
-    function createWordRequest(uint256 payment) internal returns (bytes32) {
+    function requestStartGame(uint256 payment) public returns (bytes32) {
         // newRequest takes a JobID, a callback address, and callback function as input
-        Chainlink.Request memory req = buildChainlinkRequest(CHAINLINK_JOB_ID, this, this.fulfill.selector);
+        Chainlink.Request memory req = buildChainlinkRequest(CHAINLINK_JOB_ID, this, this.fullfillStartGame.selector);
         req.add("url", url);
         req.add("path", path);
-        bytes32 requestId = sendChainlinkRequestTo(chainlinkOracleAddress(), req, payment);
+        bytes32 requestId = sendChainlinkRequest(req, payment);
+        //requestId will point to the player and the game address(which is pending)
+        requestIdToGame[requestId] = Game(msg.sender, address(0));
+        //return request id as to check if we've gotten a game back
         return requestId;
     }
 
-    function fulfill(bytes32 _requestId, bytes32 _data)
+    function fullfillStartGame(bytes32 _requestId, bytes32 _data)
         public
         recordChainlinkFulfillment(_requestId)
         returns (bytes32) {
-        return _data;
+            require(requestIdToGame[_requestId] != 0, "Id does not exist");
+
+            //get the game instance
+            Game storage gameInstance = requestIdToGame[_requestId];
+            //create the new hangman with the word(data)
+            Hangman game = new Hangman(_data, _data.length);
+            //save game into the request
+            gameInstance.game = game;
+            //update the owner of the game
+            game.transferOwnership(gameInstance.player);
     }
 
     function withdrawLink() public onlyOwner {
@@ -44,12 +65,5 @@ contract StartGame is ChainlinkClient, Ownable {
 
     function cancelRequest(bytes32 _requestId, uint256 _payment, bytes4 _callbackFunctionId, uint256 _expiration) public onlyOwner {
         cancelChainlinkRequest(_requestId, _payment, _callbackFunctionId, _expiration);
-    }
-
-    function createHangmanContract() public returns (address) {
-        bytes memory word = "cheese";
-        Hangman game = new Hangman(word, word.length);
-        game.transferOwnership(msg.sender);
-        return address(game);
     }
 }
--- test/StartGame.test.js
+++ test/StartGame.test.js
@@ -1,5 +1,8 @@
 const StartGame = artifacts.require("StartGame");
 const Hangman = artifacts.require("Hangman");
+const Oracle = artifacts.require("Oracle");
+const LinkToken = artifacts.require("LinkToken");
+const MockContract = artifacts.require("MockContract");
 const helper = require('ganache-time-traveler');
 const truffleAssert = require('truffle-assertions');
 const utils = require('./utils.js');
@@ -10,13 +13,26 @@ const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
 contract('StartGame', async (accounts) => {
 
   let startGame;
+  let mockLinkToken;
+  let mockOracle;
   let linkTokenAddress = accounts[2];
   let oracleAddress = accounts[3];
   let url = "https://en.wikipedia.org/api/rest_v1/page/random/title";
   let path = "items[0].title";
 
   before('deploy StartGame', async() => {
-      startGame = await StartGame.new(linkTokenAddress, oracleAddress, url, path);
+      let linkTokenTemplate = await LinkToken.new();
+      let mockLinkToken = await MockContract.new();
+
+      let oracleTemplate = await Oracle.new(0);
+      let mockOracle = await MockContract.new();
+
+      //mock LinkToken.transferAndCall()
+      let mockLink_transferAndCall = 
+        linkTokenTemplate.contract.methods.transferAndCall(0, 0, 0).encodeABI();
+      await mockLinkToken.givenMethodReturnBool(mockLink_transferAndCall, true);
+
+      startGame = await StartGame.new(mockLinkToken.address, mockOracle.address, url, path);
   });
 
   describe("Test initial values", async () => {
@@ -33,18 +49,24 @@ contract('StartGame', async (accounts) => {
     })
   });
 
-  describe("Test createHangmanContact", async () => {
+  describe("Test creatHangmanContact", async () => {
     it("Test createHangmanContract does not return empty address", async() => {
+        let startGame = await StartGame.new();
         let address = await startGame.createHangmanContract.call();
         assert.notEqual(address, EMPTY_ADDRESS, "address is not the null address");
     });
     
     it("Test owner of deployed contact", async() => {
-        let address = await startGame.createHangmanContract.call();
-        let trx = await startGame.createHangmanContract();
-        let hangmanContract = await Hangman.at(address);
-        let owner = await hangmanContract.owner.call();
-        assert.equal(owner, accounts[0], "Is owner should be true");
+        let startGame = await StartGame.new();
+        let requestId = await startGame.requestStartGame.call();
+
+        console.log(requestId);
+
+        let trx = await startGame.requestStartGame();;
+
+        //let hangmanContract = await Hangman.at(address);
+        //let isOwner = await hangmanContract.isOwner.call();
+        //assert.equal(isOwner, true, "Is owner should be true");
     });
   });
 });

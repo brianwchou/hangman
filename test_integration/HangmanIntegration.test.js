b9dd05c68b3e4cfb26b6c328deb92ff11d7843bd
Ethan Wessel <ejwessel@gmail.com>
2019-09-14 21:10:16 -0400

    correct payment

--- test_integration/HangmanIntegration.test.js
+++ test_integration/HangmanIntegration.test.js
@@ -5,18 +5,17 @@ const LinkToken = artifacts.require("LinkToken");
 const truffleAssert = require('truffle-assertions');
 const utils = require('./utils.js');
 const web3 = utils.getWeb3();
+const BigNumber = require('bignumber.js');
+
+const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
+const url = "https://en.wikipedia.org/api/rest_v1/page/random/title";
+const path = "items[0].title";
 
 //ROPSTEN TESTNET ADDRESS
 const chainlinkTokenAddress = "0x20fE562d797A42Dcb3399062AE9546cd06f63280";
 const chainlinkOracleAddress = "0xc99B3D447826532722E41bc36e644ba3479E4365";
-const url = "https://en.wikipedia.org/api/rest_v1/page/random/title";
-const path = "items[0].title";
-
-const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
 const CHAINLINK_HTTP_GET_JOB_ID = "96bf1a27492142b095a8ada21631ebd0"; //this is the testnet jobid
 const PAYMENT = 1;
-const url = "https://en.wikipedia.org/api/rest_v1/page/random/title";
-const path = "items[0].title";
 
 contract('Hangman Integration Tests', async (accounts) => {
   let hangmanFactory;
@@ -25,13 +24,14 @@ contract('Hangman Integration Tests', async (accounts) => {
   //probably need to use truffle-hdwallet-provider
 
   before('deploy HangmanFactory', async() => {
-      hangmanFactory = await HangmanFactory.new(
-        chainlinkTokenAddress,
-        chainlinkOracleAddress,
-        url,
-        path
-      );
-      //hangmanFactory = await HangmanFactory.at("0xe6dae7329f72C8C2E114cf4CbAb84D13600607CC");
+//      hangmanFactory = await HangmanFactory.new(
+//        chainlinkTokenAddress,
+//        chainlinkOracleAddress,
+//        url,
+//        path
+//      );
+      hangmanFactory = await HangmanFactory.at("0x4d1d55e1f916d388ae5d236610dbf7c3c58d05a6");
+      //transfer link to hangman factory address at the value it's going to use
       console.log("HangmanFactory Address: " + hangmanFactory.address)
   });
 
@@ -45,11 +45,28 @@ contract('Hangman Integration Tests', async (accounts) => {
         let val = await hangmanFactory.path.call();
         assert.equal(val, path, "path not properly set");
     })
+
+    it("Test LinkToken", async() => {
+        let val = await hangmanFactory.linkToken.call();
+        assert.equal(val, chainlinkTokenAddress, "token not properly set");
+    })
+
+    it("Test Oracle", async() => {
+        let val = await hangmanFactory.oracle.call();
+        assert.equal(val, chainlinkOracleAddress, "oracle not properly set");
+    })
   });
 
   describe("Test factory and hangman game", async () => {
     it("Test requestCreateGame is successful in requesting to start a game", async() => {
-        let trx = await hangmanFactory.requestCreateGame(web3.fromAscii(CHAINLINK_HTTP_GET_JOB_ID), PAYMENT);
+        
+        //give approval to factory
+        let token = await LinkToken.at(chainlinkTokenAddress);
+        let decimals = (await token.decimals.call()).toNumber();
+        let paymentAmount = new BigNumber(PAYMENT * Math.pow(10, decimals));
+        await token.approve(hangmanFactory.address, paymentAmount);
+
+        let trx = await hangmanFactory.requestCreateGame(web3.fromAscii(CHAINLINK_HTTP_GET_JOB_ID), paymentAmount);
 
         //listen for event and capture the requestId
         truffleAssert.eventEmitted(trx, 'RequestCreateGame', (e) => {

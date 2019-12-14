b1a98f69dc13919c5b167391804d154cbf30f1f2
Ethan Wessel <ejwessel@gmail.com>
2019-12-14 18:19:56 -0500

    game templates

---
+++ web/src/Game.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useContext } from 'react';	
+import { Context } from './context.js';
+
+function Game() {
+  const [context, setContext] = useContext(Context)
+}
+
+return {
+  <div>
+  </div>
+}
---
+++ web/src/GameOptions.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useContext } from 'react';	
+import { Context } from './context.js';
+
+function GameOptions() {
+  const [context, setContext] = useContext(Context)
+}
+
+return {
+  <div>
+  </div>
+}

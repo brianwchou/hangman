5399da9cd6ee682dc7955428f84b869778ae7dc0
Ethan Wessel <ejwessel@gmail.com>
2019-12-15 11:30:09 -0500

    update game options location

--- web/src/GameOptions.js
+++ web/src/GameOptions.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect, useContext } from 'react';	
 import { Context } from './context.js';
+import {Grid, Paper, Typography, Button, List, ListItem} from '@material-ui/core';
 
 function GameOptions() {
   const [context, setContext] = useContext(Context)
 
   return (
     <div>
-      {/*
-        Connect Wallet
-        Player Address
-        Start Game Button
-      */}
+      <Grid item>
+        <Paper elevation={0}>
+          <List>
+            <ListItem>address: 0xb893D8F6779842959C1dfC3095b1c62ceAA16703</ListItem>
+            <ListItem selected>start new game</ListItem>
+            <ListItem>play unfinished contract</ListItem>
+          </List>
+        </Paper>
+        <Button variant='contained' color='primary'>Connect Wallet</Button>
+      </Grid>
     </div>
   )
 }
--- web/src/StartScreen.js
+++ web/src/StartScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Context } from './context.js';
 import GameOptions from './GameOptions.js';
 import { makeStyles } from '@material-ui/core/styles';
-import {Grid, Paper, Typography, Button, List, ListItem} from '@material-ui/core';
+import {Grid, Paper, Typography} from '@material-ui/core';
 
 function StartScreen() {
   const [context, setContext] = useContext(Context);
@@ -24,16 +24,7 @@ function StartScreen() {
             </Typography>
           </Paper>
         </Grid>
-        <Grid item>
-          <Paper elevation={0}>
-            <List>
-              <ListItem>address: 0xb893D8F6779842959C1dfC3095b1c62ceAA16703</ListItem>
-              <ListItem selected>start new game</ListItem>
-              <ListItem>play unfinished contract</ListItem>
-            </List>
-          </Paper>
-          <Button variant='contained' color='primary'>Connect Wallet</Button>
-        </Grid>
+        <GameOptions/>
       </Grid>
     </div>
   )

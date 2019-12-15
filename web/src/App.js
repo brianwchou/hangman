fd871674af66d6fa6107568ea578c280322152ea
brian <bchou515@gmail.com>
2019-12-14 23:30:35 -0500

    change indentation

--- web/src/App.js
+++ web/src/App.js
@@ -3,14 +3,12 @@ import { Context } from './context';
 import StartScreen from './StartScreen.js';
 import GameScreen from './GameScreen.js';
 
-
 function App() {
-
   const [context, setContext] = useContext(Context);
 
   return (
     <div>
-        <StartScreen />
+      <StartScreen />
     </div>
   );
 }
--- web/src/StartScreen.js
+++ web/src/StartScreen.js
@@ -1,31 +1,33 @@
 import React, { useState, useEffect, useContext } from 'react';	
 import { Context } from './context.js';
 import GameOptions from './GameOptions.js';
+import { makeStyles } from '@material-ui/core/styles';
 import {Grid, Paper, Typography, Button} from '@material-ui/core';
 
 function StartScreen() {
-  const [context, setContext] = useContext(Context)
+  const [context, setContext] = useContext(Context);
+
   return (
     <div>
-        <Grid   
+      <Grid   
         container 
         direction='column'
         justify='center' 
         spacing={3}
         alignItems='center'
         style={{ minHeight: '100vh' }}
-        >
-            <Grid item>
-                <Paper>
-                    <Typography variant="h3" component="h5">
-                        Hangman
-                    </Typography>
-                </Paper>
-            </Grid>
-            <Grid item>
-                <Button variant='contained' color='primary'>click me</Button>
-            </Grid>
+      >
+        <Grid item>
+          <Paper elevation={0}>
+            <Typography variant="h1">
+              Hangman
+            </Typography>
+          </Paper>
         </Grid>
+        <Grid item>
+          <Button variant='contained' color='primary'>Connect Wallet</Button>
+        </Grid>
+      </Grid>
     </div>
   )
 }

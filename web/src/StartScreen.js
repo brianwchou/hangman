0ec6ca1b64f05a99cdba6453dbe8f37f9988c674
brian <bchou515@gmail.com>
2019-12-14 23:50:07 -0500

    add options

--- web/src/StartScreen.js
+++ web/src/StartScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Context } from './context.js';
 import GameOptions from './GameOptions.js';
 import { makeStyles } from '@material-ui/core/styles';
-import {Grid, Paper, Typography, Button} from '@material-ui/core';
+import {Grid, Paper, Typography, Button, List, ListItem} from '@material-ui/core';
 
 function StartScreen() {
   const [context, setContext] = useContext(Context);
@@ -25,6 +25,13 @@ function StartScreen() {
           </Paper>
         </Grid>
         <Grid item>
+          <Paper elevation={0}>
+            <List>
+              <ListItem>address: 0xb893D8F6779842959C1dfC3095b1c62ceAA16703</ListItem>
+              <ListItem selected>start new game</ListItem>
+              <ListItem>play unfinished contract</ListItem>
+            </List>
+          </Paper>
           <Button variant='contained' color='primary'>Connect Wallet</Button>
         </Grid>
       </Grid>

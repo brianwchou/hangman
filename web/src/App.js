fb5e361d5095977bf63e28ec04e312b7a35fc05a
brian <bchou515@gmail.com>
2019-12-14 19:53:20 -0500

    StartScreen UI components

--- web/src/App.js
+++ web/src/App.js
@@ -3,16 +3,14 @@ import { Context } from './context';
 import StartScreen from './StartScreen.js';
 import GameScreen from './GameScreen.js';
 
+
 function App() {
 
   const [context, setContext] = useContext(Context);
 
   return (
     <div>
-      {/*
-        Start Screen
-        Game Screen
-      */}
+        <StartScreen />
     </div>
   );
 }
--- web/src/StartScreen.js
+++ web/src/StartScreen.js
@@ -1,15 +1,31 @@
 import React, { useState, useEffect, useContext } from 'react';	
 import { Context } from './context.js';
 import GameOptions from './GameOptions.js';
-
+import {Grid, Paper, Typography, Button} from '@material-ui/core';
 
 function StartScreen() {
   const [context, setContext] = useContext(Context)
   return (
     <div>
-      {/*
-        Game Options
-      */}
+        <Grid   
+        container 
+        direction='column'
+        justify='center' 
+        spacing={3}
+        alignItems='center'
+        style={{ minHeight: '100vh' }}
+        >
+            <Grid item>
+                <Paper>
+                    <Typography variant="h3" component="h5">
+                        Hangman
+                    </Typography>
+                </Paper>
+            </Grid>
+            <Grid item>
+                <Button variant='contained' color='primary'>click me</Button>
+            </Grid>
+        </Grid>
     </div>
   )
 }

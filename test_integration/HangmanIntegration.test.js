bc7c36d4a6c5f1e80238db46033617c53bbe6ea4
Ethan Wessel <ethan.wessel@fluidity.io>
2020-03-21 10:15:49 -0400

    update job id

--- test_integration/HangmanIntegration.test.js
+++ test_integration/HangmanIntegration.test.js
@@ -16,7 +16,7 @@ const path = ["items", "0", "title"];
 //ROPSTEN TESTNET ADDRESS
 const chainlinkTokenAddress = "0x20fE562d797A42Dcb3399062AE9546cd06f63280";
 const chainlinkOracleAddress = "0xc99B3D447826532722E41bc36e644ba3479E4365";
-const CHAINLINK_HTTP_GET_JOB_ID = "96bf1a27492142b095a8ada21631ebd0";
+const CHAINLINK_HTTP_GET_JSON_PARSE_JOB_ID = "76ca51361e4e444f8a9b18ae350a5725";
 const PAYMENT = 1;
 
 contract('Hangman Integration Tests', async (accounts) => {
@@ -72,7 +72,7 @@ contract('Hangman Integration Tests', async (accounts) => {
         let paymentAmount = new BigNumber(PAYMENT * Math.pow(10, decimals));
         await token.approve(hangmanFactory.address, paymentAmount);
 
-        let trx = await hangmanFactory.requestCreateGame(web3.fromAscii(CHAINLINK_HTTP_GET_JOB_ID), paymentAmount);
+        let trx = await hangmanFactory.requestCreateGame(web3.fromAscii(CHAINLINK_HTTP_GET_JSON_PARSE_JOB_ID), paymentAmount);
 
         //listen for event and capture the requestId
         truffleAssert.eventEmitted(trx, 'RequestCreateGame', (e) => {

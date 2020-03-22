c66b095940d2f8f98447b774901e1f591c38d225
Ethan Wessel <ethan.wessel@fluidity.io>
2020-03-22 14:19:11 -0400

    spacing between items

--- web/src/GameScreen.js
+++ web/src/GameScreen.js
@@ -13,29 +13,30 @@ function GameScreen(setScreen, classes) {
 
   return (
     <div className={classes.root}>
-      <Grid container
-        justify='center'
-        spacing={1}
-        alignItems='center'
-        style={{ minHeight: '100vh' }}
-      >
+      <Grid container justify='center' spacing={1} alignItems='center' style={{ minHeight: '100vh' }}>
+
         <Grid item xs={6}>
           <Paper className={classes.paper}>
-          <img className={classes.img} src='https://d1nhio0ox7pgb.cloudfront.net/_img/i_collection_png/512x512/plain/guillotine.png' />
+          <img className={classes.img} 
+            src='https://d1nhio0ox7pgb.cloudfront.net/_img/i_collection_png/512x512/plain/guillotine.png'
+          />
             <Typography>
               Hangman Word Here
             </Typography>
          </Paper>
         </Grid>
-        <Grid item xs={6}>
+
+        <Grid item xs={6} spacing={4}>
           <Paper className={classes.paper}>
             <Grid container justify='center' direction='column' spacing={1} alignItems='center' style={{ minHeight: '50vh' }}>
               <Typography>
                 Guesses Left 50/50
               </Typography>
+
               <Typography>
                 Used Characters: a b c d e f g h i j k l m n o p q r s t u v w x y z
               </Typography>
+
               <Grid container justify='left' direction='row' spacing={1} alignItems='center'>
                 <Grid item>
                   <TextField id="outlined-basic" label="Guess Word" variant="outlined" />
@@ -44,6 +45,7 @@ function GameScreen(setScreen, classes) {
                   <Button variant='contained' color='primary' onClick={submitWord}>Submit Word</Button>
                 </Grid>
               </Grid>
+
               <Grid container justify='left' direction='row' spacing={1} alignItems='center'>
                 <Grid item>
                   <TextField id="outlined-basic" label="Guess Character" variant="outlined" />
@@ -52,6 +54,7 @@ function GameScreen(setScreen, classes) {
                   <Button variant='contained' color='primary' onClick={submitChar}>Submit Char</Button>
                 </Grid>
               </Grid>
+
             </Grid>
           </Paper>
         </Grid>

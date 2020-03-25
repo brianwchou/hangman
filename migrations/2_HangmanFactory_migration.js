9179c639242592a5e0870961bda7582d4fac01d7
brian <bchou515@gmail.com>
2020-03-25 19:53:57 -0400

    migrations file cleanup

--- migrations/2_HangmanFactory_migration.js
+++ migrations/2_HangmanFactory_migration.js
@@ -2,13 +2,8 @@ const Hangman = artifacts.require("Hangman");
 const ethers = require("ethers");
 
 module.exports = async function(deployer) {
-
-  await deployer.deploy(
-    Hangman
-  );
-  
+  await deployer.deploy(Hangman);
   let instance = await Hangman.deployed()
-
   await instance.setSolution(ethers.utils.toUtf8Bytes("hello"), 5);
 };
 

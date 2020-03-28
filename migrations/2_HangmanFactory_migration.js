fadab2a81e92031c709d527d814f217aae51c014
brian <bchou515@gmail.com>
2020-03-28 19:22:21 -0400

    change migrations value back

--- migrations/2_HangmanFactory_migration.js
+++ migrations/2_HangmanFactory_migration.js
@@ -4,6 +4,6 @@ const ethers = require("ethers");
 module.exports = async function(deployer) {
   await deployer.deploy(Hangman);
   let instance = await Hangman.deployed()
-  await instance.setSolution(ethers.utils.toUtf8Bytes("hello"), 2);
+  await instance.setSolution(ethers.utils.toUtf8Bytes("hello"), 5);
 };
 

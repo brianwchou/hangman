ec1977ba035bc2d87e68b88632d11bf2e858e379
brian <bchou515@gmail.com>
2020-03-30 14:07:08 -0400

    change value in factory migration

--- migrations/1_HangmanFactory_migration.js
+++ migrations/1_HangmanFactory_migration.js
@@ -6,13 +6,13 @@ module.exports = async function(deployer) {
   const chainlinkTokenAddress = "0x20fE562d797A42Dcb3399062AE9546cd06f63280";
   const chainlinkOracleAddress = "0xc99B3D447826532722E41bc36e644ba3479E4365";
   const url = "https://en.wikipedia.org/api/rest_v1/page/random/title";
-  const path = "items[0].title";
+  const path = ["items", "0", "title"];
 
-//  await deployer.deploy(
-//    HangmanFactory,
-//    chainlinkTokenAddress, 
-//    chainlinkOracleAddress, 
-//    url, 
-//    path
-//  );
+ await deployer.deploy(
+   HangmanFactory,
+   chainlinkTokenAddress, 
+   chainlinkOracleAddress, 
+   url, 
+   path
+ );
 };
